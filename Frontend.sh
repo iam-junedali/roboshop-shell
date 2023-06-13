c20dc4c51c2b6f32faccd639adf764bbb8c98c0f
junedali1995 <junedali.sk@gmail.com>
2023-06-13 09:40:44 +0530

    Aligned message format

--- Frontend.sh
+++ Frontend.sh
@@ -1,18 +1,18 @@
-echo -e "\e[31mInstalling Nginx Server\-e[0m"
+echo -e "\e[31m Installing Nginx Server\ -e[0m"
 yum install nginx -y
 
-echo -e "\e[32mRemoving Default Content from Nginx Server\-e[0m"
+echo -e "\e[32m Removing Default Content from Nginx Server \-e[0m"
 rm -rf /usr/share/nginx/html/*
 
-echo -e "\e[33mInstalling Frontend Content\-e[0m"
+echo -e "\e[33m Installing Frontend Content \-e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
-echo -e "\e[34munzip the Frontend content\-e[0m"
+echo -e "\e[34m unzip the Frontend content \-e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-echo -e "\e[35mEnable Nginx Server\-e[0m"
+echo -e "\e[35m Enable Nginx Server \-e[0m"
 systemctl enable nginx
 
-echo -e "\e[35mRestart Nginx Server\-e[0m"
+echo -e "\e[35m Restart Nginx Server \-e[0m"
 systemctl restart nginx
