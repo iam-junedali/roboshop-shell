1be333221917d7f3a2ed923de93dfb9a1cd16848
junedali1995 <junedali.sk@gmail.com>
2023-07-03 21:52:00 +0530

    updated

--- Frontend.sh
+++ Frontend.sh
@@ -5,8 +5,8 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip >>/tmp/roboshop.log
 
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip >>/tmp/roboshop.log
+unzip /tmp/frontend.zip
 
-systemctl enable nginx >>/tmp/roboshop.log
+systemctl enable nginx
 
-systemctl restart nginx >>/tmp/roboshop.log
+systemctl restart nginx
