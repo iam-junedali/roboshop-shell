eac5e291262128012e405fd50369a3b6ccf8d81f
junedali1995 <junedali.sk@gmail.com>
2023-07-03 21:57:50 +0530

    update project

--- mongodb.sh
+++
@@ -1,9 +0,0 @@
-echo -e "\e[31m Copy mongodb.repo file \ -e[0m"
-cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
-
-echo -e "\e[31m Install Mongodb \ -e[0m"
-yum install mongodb-org -y &>>/tmp/roboshop.log
-
-echo -e "\e[31m Restart Mongodb \ -e[0m"
-systemctl enable mongod &>>/tmp/roboshop.log
-systemctl restart mongod &>>/tmp/roboshop.log
