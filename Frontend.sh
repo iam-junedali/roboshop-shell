5a90470578f08b58133daee7596a8c6a5f94a0ff
junedali1995 <junedali.sk@gmail.com>
2023-07-04 15:25:00 +0530

    updated

--- Frontend.sh
+++ Frontend.sh
@@ -1,12 +1,12 @@
-yum install nginx -y >/tmp/roboshop.log
+yum install nginx -y
 
-rm -rf /usr/share/nginx/html/* >/tmp/roboshop.log
+rm -rf /usr/share/nginx/html/*
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip >>/tmp/roboshop.log >/tmp/roboshop.log
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip >/tmp/roboshop.log
+unzip /tmp/frontend.zip
 
-systemctl enable nginx >/tmp/roboshop.log
+systemctl enable nginx
 
-systemctl restart nginx >/tmp/roboshop.log
+systemctl restart nginx
