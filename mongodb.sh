1292e33dfb36435dd53255e1e0dd3a144c33536b
junedali1995 <junedali.sk@gmail.com>
2023-09-18 19:37:48 +0530

    File Updated

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,9 @@
 cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
 
-yum install mongodb-org -y
+dnf install mongodb-org -y &>>/tmp/roboshop.log
 
-systemctl enable mongod
-systemctl start mongod
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>/tmp/roboshop.log
+
+systemctl enable mongod &>>/tmp/roboshop.log
+systemctl start mongod &>>/tmp/roboshop.log
 
