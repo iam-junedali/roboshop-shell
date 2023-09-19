fa6d0d332cc89bd3ed28ab8a7da6a7059f25968a
junedali1995 <junedali.sk@gmail.com>
2023-09-19 11:56:43 +0530

    catalogue is updated

--- catalogue.sh
+++ catalogue.sh
@@ -1,17 +1,17 @@
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-dnf install nodejs -y &>>/tmp/roboshop.log
+dnf install nodejs -y
 
 useradd roboshop
 
 mkdir /app
 
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
-unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
+unzip /tmp/catalogue.zip
 
 cd /app
-npm install &>>/tmp/roboshop.log
+npm install
 
 cp catalogue.service /etc/systemd/system/catalogue.service
 
@@ -20,9 +20,9 @@ systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
 
-cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
+cp mongodb.repo /etc/yum.repos.d/mongo.repo
 
-dnf install mongodb-org-shell -y &>>/tmp/roboshop.log
+dnf install mongodb-org-shell -y
 
 mongo --host mongodb-dev.joyousgroups.com </app/schema/catalogue.js
 
