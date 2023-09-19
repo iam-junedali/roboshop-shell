35a8d206bda007c2e392a29f1b2ba5fbb36675f9
junedali1995 <junedali.sk@gmail.com>
2023-09-19 11:49:29 +0530

    MongoDB is updated

--- mongodb.sh
+++ mongodb.sh
@@ -1,9 +1,11 @@
-cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
+cp mongodb.repo /etc/yum.repos.d/mongo.repo
 
-dnf install mongodb-org -y &>>/tmp/roboshop.log
+dnf install mongodb-org -y
 
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>/tmp/roboshop.log
+systemctl enable mongod
+systemctl start mongod
 
-systemctl enable mongod &>>/tmp/roboshop.log
-systemctl start mongod &>>/tmp/roboshop.log
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+
+systemctl restart mongod
 
